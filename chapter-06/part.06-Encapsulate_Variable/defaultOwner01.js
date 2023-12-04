9b0e5c6266fb58c06e774b8183dc0ee29c72b6b3
87parkjw <87parkjw@ktown4u.com>
2023-12-05 02:40:44 +0900

    .

--- chapter-06/part.06-Encapsulate_Variable/defaultOwner01.js
+++ chapter-06/part.06-Encapsulate_Variable/defaultOwner01.js
@@ -6,3 +6,11 @@ spaceship.owner = defaultOwner;
 defaultOwner = { firstName: '레베카', lastName: '파슨스' };
 
 module.exports = { defaultOwner };
+
+/*
+const owner2 = getDefaultOwner();
+console.log('파울러', owner1.lastName, '처음 값 확인');
+const owner2 = getDefaultOwner();
+owner2.lastName = '파슨스';
+console.log('파슨스', owner1.lastName, 'owner2를 변경한 후');
+ */
