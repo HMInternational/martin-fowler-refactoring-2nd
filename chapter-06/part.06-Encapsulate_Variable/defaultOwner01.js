762bd99d443eda93dd573fde42a8943758be1d99
87parkjw <87parkjw@ktown4u.com>
2023-12-05 02:26:10 +0900

    .

--- chapter-06/part.06-Encapsulate_Variable/defaultOwner01.js
+++ chapter-06/part.06-Encapsulate_Variable/defaultOwner01.js
@@ -1,11 +1,8 @@
+let spaceship = {};
 let defaultOwner = { firstName: '마틴', lastName: '파울러' };
 
-function getDefaultOwner() {
-  return defaultOwner;
-}
+spaceship.owner = defaultOwner;
 
-function setDefaultOwner(arg) {
-  defaultOwner = arg;
-}
+defaultOwner = { firstName: '레베카', lastName: '파슨스' };
 
-module.exports = { defaultOwner, getDefaultOwner, setDefaultOwner };
+module.exports = { defaultOwner };
